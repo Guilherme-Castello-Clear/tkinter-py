fe0ecd6eb3e78f131d65bd90356a395716dcd1ad
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-01 13:12:53 -0300

    Feat: Comparing TKinter and Turtle

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("First GUI tk")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="Label test", font=("Arial", 24, "bold"))
+my_label.pack()
+
+import turtle
+
+tim = turtle.Turtle()
+tim.write("test")
+
+
+window.mainloop()
