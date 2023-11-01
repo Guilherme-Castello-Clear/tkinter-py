e92698a9e152c1353e5d58216b1829a603cd8ce7
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-01 13:50:32 -0300

    Feat: Usign args and kwargs

--- playground.py
+++ playground.py
@@ -1,7 +1,24 @@
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+# print(add(3, 5, 7, 8))
 
-print(add(3, 5, 7, 8))
+def calculate(n, **kwargs):
+    # for key, value in kwargs.item()
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw.get("model")
+
+my_car = Car(make="Nissan")
+print(my_car.model)
