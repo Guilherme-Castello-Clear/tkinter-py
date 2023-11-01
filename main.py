d4b336a8d4007fd9a594a80319dc33339d131acc
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-01 13:56:41 -0300

    Feat: Using Input and Button

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("First GUI tk")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Label test", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = Label(text="Label test", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+my_label["text"] = "New text"
+my_label.config(text="New text")
 
 
+def button_clicked():
+    print("Clicked")
+    button.config(text="Clicked")
+    print(input.get())
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
